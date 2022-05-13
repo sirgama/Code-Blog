b18f73a6c3aacec02113a960e657f7db32fa1a9c
sirgama <sirengogamaliel@gmail.com>
2022-05-13 22:48:58 +0300

    database setup

--- codeblog/__init__.py
+++ codeblog/__init__.py
@@ -3,9 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "hello-world"
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     
     from .views import views
@@ -14,4 +20,11 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
     
+    create_database(app)
+    
     return app
+
+def create_database(app):
+    if not path.exists("codeblog/" + DB_NAME):
+        db.create_all(app=app)
+        print('Created database')
