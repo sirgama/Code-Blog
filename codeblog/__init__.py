1148860c810b88a3239875667d6a448890860c5e
sirgama <sirengogamaliel@gmail.com>
2022-05-13 17:33:55 +0300

    create views blueprint

--- codeblog/__init__.py
+++ codeblog/__init__.py
@@ -7,4 +7,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "hello-world"
     
+    
+    from .views import views
+    
+    
+    app.register_blueprint(views, url_prefix="/")
+    
     return app
