fbaa56898987d81ff6da8ec83c64187f7ea421bd
sirgama <sirengogamaliel@gmail.com>
2022-05-13 22:21:37 +0300

    create login form

--- codeblog/auth.py
+++ codeblog/auth.py
@@ -1,16 +1,16 @@
-from flask  import Blueprint, render_template
+from flask  import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login")
 def login():
-    return "login"
+    return render_template('login.html')
 
 @auth.route("/sign_up")
 def sign_up():
-    return "sign_up"
+    return render_template('signup.html')
 
 @auth.route("/logout")
 def logout():
-    return "logout"
+    return redirect(url_for('views.home'))
