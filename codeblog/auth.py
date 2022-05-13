1e473c6671f30e4856269106fa59be0f53026990
sirgama <sirengogamaliel@gmail.com>
2022-05-13 22:42:35 +0300

    getting form data from the login and signup

--- codeblog/auth.py
+++ codeblog/auth.py
@@ -1,14 +1,21 @@
-from flask  import Blueprint, render_template, redirect, url_for
+from crypt import methods
+from flask  import Blueprint, render_template, redirect, request, url_for
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
     return render_template('login.html')
 
-@auth.route("/sign_up")
+@auth.route("/sign_up", methods=['GET', 'POST'])
 def sign_up():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    password = request.form.get('password1')
     return render_template('signup.html')
 
 @auth.route("/logout")
