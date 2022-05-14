e5bd8420d9bd38b7fd9a1b64f43c97709a404abe
sirgama <sirengogamaliel@gmail.com>
2022-05-14 08:50:35 +0300

    views file restructure

--- codeblog/views.py
+++ codeblog/views.py
@@ -1,10 +1,29 @@
-from flask  import Blueprint, render_template
-from . import db
-from .models import User
+from flask  import abort, render_template, redirect, url_for, flash, request
+from codeblog import app, db, mail
+from codeblog.models import User
+from flask_login import login_required, logout_user, login_user
 
-views = Blueprint("views", __name__)
 
 
-@views.route("/home")
+@app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+      
+        
+    return render_template('login.html')
+    
+
+@app.route("/sign_up", methods=['GET', 'POST'])
+def sign_up():
+    
+        
+    return render_template('signup.html')
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
