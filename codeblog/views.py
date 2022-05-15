2b1d8f386a73bbe9b7bffb38af2fde2198c6ec36
sirgama <sirengogamaliel@gmail.com>
2022-05-15 15:15:09 +0300

    create login form template

--- codeblog/views.py
+++ codeblog/views.py
@@ -1,7 +1,9 @@
 from flask  import abort, render_template, redirect, url_for, flash, request
 from codeblog import app, db, mail
-from codeblog.models import User
+from codeblog.models import User, Blog, Comment, Like, Dislike
+from codeblog.forms import LoginForm, RegistrationForm, RequestResetForm, ResetPasswordForm, UpdateForm,CommentForm
 from flask_login import login_required, logout_user, login_user, current_user
+from flask_mail import Message
 
 
 
@@ -18,7 +20,16 @@ def login():
 
 @app.route("/sign_up", methods=['GET', 'POST'])
 def sign_up():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Account created successfully for {form.username.data}!", 'success')
+        return redirect(url_for('login'))
         
     return render_template('signup.html')
 
