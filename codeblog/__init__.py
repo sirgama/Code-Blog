3135f2df85395da24111fe04bf1c0eb3bf3dbc57
sirgama <sirengogamaliel@gmail.com>
2022-05-17 09:04:31 +0300

    Updated migrations

--- codeblog/__init__.py
+++ codeblog/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_ckeditor import CKEditor
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -12,6 +13,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///codeblog.db'
 
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
